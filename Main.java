e381ba6f69f8d6599a35f82ee6dfdc6890a77a3f
F1artem <103590660+F1artem@users.noreply.github.com>
2022-04-21 11:12:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+    Person man=new Person();
+    Person woman=new Person(15,"Маша","5 метров");
+    man.setFullName("Юра");
+    man.setAge(21);
+    man.setWalk("2 метра");
+    man.talk();
+    man.walk();
+    System.out.println();
+    woman.talk();
+    woman.walk();
+
+    }
+}
